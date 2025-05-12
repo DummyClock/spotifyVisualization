accd4cdacf8de23195a8997eb3e9aea02963a62a
Elias S. <92802691+DummyClock@users.noreply.github.com>
2025-05-12 06:06:59 -0400

    Messing around with displays & tooltip!

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ class Dashboard extends Component {
       .min(min_year)
       .max(max_year)
       .step(1)
-      .width(300)
+      .width(500)
       .ticks((max_year - min_year) / 10)
       .tickFormat(d3.format(""))
       .default([max_year, min_year])
@@ -90,19 +90,18 @@ class Dashboard extends Component {
     const data = this.state.scatter_filtered_data;
     
     // Append canvas
-    var svg = d3.select('.scatterplot')
-      .attr("width", innerWidth + margin.left + margin.right)
-      .attr("height", innerHeight + margin.top + margin.bottom + 40) 
-    var innerChart = svg.selectAll(".inner_chart")
-      .data([null])
-      .join("g")
-      .attr("class", "inner_chart")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-    
+    const svg = d3.select('.scatterplot')
+        .attr("width", innerWidth + margin.left + margin.right)
+        .attr("height", innerHeight + margin.top + margin.bottom) 
+    let innerChart = svg.selectAll(".inner_chart")
+        .data([null])
+        .join("g")
+        .attr("class", "inner_chart")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`)  
 
     // X-axis + Y-axis
-    const xValues = data.map(d => d[x1]);
-    const yValues = data.map(d => d[y1]);
+    const xValues = data.map(d => d[x1])
+    const yValues = data.map(d => d[y1])
 
     const xPadding = 0.05 * d3.max(xValues);
     const xScale = d3.scaleLinear()
@@ -128,12 +127,9 @@ class Dashboard extends Component {
     // Add labels for the axis lines
     innerChart.selectAll(".x-axis-label")
       .data([null])
-      .join("text")
       .attr("class", "x-axis-label")
       .attr("x", innerWidth / 2) 
       .attr("y", innerHeight + margin.bottom ) 
-      .attr("text-anchor", "middle") 
-      .text(x1);
     innerChart.selectAll(".y-axis-label")
       .data([null])
       .join("text")
@@ -164,33 +160,71 @@ class Dashboard extends Component {
         var y = yScale(point[y1]) + margin.top
         return x >= e.selection[0][0] && x <= e.selection[1][0] && y >= e.selection[0][1] && y <= e.selection[1][1]
       });
-      this.scatter(this.state.x1, this.state.y1)
-      this.setState({bar_filtered_data : filtered_data})
+      this.setState({ bar_filtered_data: filtered_data }, () => {
+      this.scatter(this.state.x1, this.state.y1); 
+      this.stack(); 
+      this.rebuildZoomSlider(); // force reset
+  });
     });
   
     d3.select('svg').call(brush)
   }
-  
 
+  rebuildZoomSlider() {
+    d3.select('.slider-streams').selectAll('*').remove(); 
+    this.createZoomSlider(); 
+  }
 
-  
+  createZoomSlider(xScale) {
+    const originalData = [...this.state.bar_filtered_data];
+    const allTrackNames = originalData.map(d => d.TrackName);
+    const minIndex = 0;
+    const maxIndex = allTrackNames.length - 1;
+    
+    const sliderRange = sliderBottom()
+      .min(minIndex)
+      .max(maxIndex)
+      .step(1)
+      .width(300)
+      .ticks(5)
+      .tickFormat(() => "")
+      .default([minIndex, maxIndex])
+      .fill('#85bb65')
+      .on('onchange', val => {             
+        const visibleTracks = allTrackNames.slice(val[0], val[1] + 1);
+        const filtered = originalData.filter(d => visibleTracks.includes(d.TrackName));  
+        this.setState({ bar_filtered_data: filtered }, () => { this.stack(false) }); // prevent slider duplicating 
+      });
+
+    
+    // Add slider to page
+    const gRange = d3.select('.slider-streams')
+        .attr('width', 900)
+        .attr('height', 100)
+        .append('g')
+        .attr('class', 'slider-g')
+        .attr('transform', 'translate(90,40)');
+
+    gRange.call(sliderRange);
+  }
+    
   stack() {
     if (!this.state.y2 || this.state.y2.length === 0 || !this.state.bar_filtered_data.length) return;
     const brushData = this.state.bar_filtered_data;
     const selectedAttr = this.state.y2.map(attr => attr.value);
 
     // Recalculate innerWidth based on current margin (important if margin changes)
-    const currentInnerWidth = 460 - margin.left - margin.right;
+    const currentInnerWidth = 600 - margin.left - margin.right;
     const currentInnerHeight = 400 - margin.top - margin.bottom;
 
     let svg = d3.select('.stackedChart');
     if (svg.empty()) {
       svg = d3.select('.stackedChart')
-        .attr("width", currentInnerWidth + margin.left + margin.right) // Use recalculated width
-        .attr("height", currentInnerHeight + margin.top + margin.bottom + margin.bottom / 6); // Use recalculated height
+        .attr("width", currentInnerWidth + margin.left + margin.right) 
+        .attr("height", currentInnerHeight + margin.top + margin.bottom + margin.bottom + 60); 
     } else {
       svg.attr("width", currentInnerWidth + margin.left + margin.right)
-        .attr("height", currentInnerHeight + margin.top + margin.bottom + margin.bottom / 6);
+        .attr("height", currentInnerHeight + margin.top + margin.bottom + margin.bottom + 60);
     }
 
     let innerChart = svg.select(".stacked_inner_chart");
@@ -229,7 +263,21 @@ class Dashboard extends Component {
       .attr("x", d => xScale(d.data.TrackName))
       .attr("y", d => yScale(d[1]))
       .attr("height", d => yScale(d[0]) - yScale(d[1]))
-      .attr("width", xScale.bandwidth());
+      .attr("width", xScale.bandwidth())
+      .on("mouseover", function(event, d) {
+        d3.select("#tooltip")
+          .style("opacity", 1)
+          .html(`<h3>Artist(s): ${d.data.ArtistName}</h3> <h4>Track: ${d.data.TrackName}</h4><br>  - ${this.parentNode.__data__.key}: ${d[1] - d[0]}`);
+      })
+      .on("mousemove", function(event) {
+          d3.select("#tooltip")
+            .style("left", (event.pageX + 10) + "px")
+            .style("top", (event.pageY - 28) + "px");
+      })
+      .on("mouseout", function() {
+          d3.select("#tooltip")
+            .style("opacity", 0);
+      });
 
     // Add Legend
     const legend = d3.select(".stacked-legend")
@@ -259,10 +307,32 @@ class Dashboard extends Component {
       xAxis = svg.append("g")
         .attr("class", "x-axis")
         .attr("transform", `translate(${margin.left}, ${currentInnerHeight + margin.top})`)
-        .call(d3.axisBottom(xScale).tickFormat(d => d.length > 10 ? d.substring(0, 10) + '...' : d));
+        .call(d3.axisBottom(xScale).tickFormat(track => {
+          const song = brushData.find(d => d.TrackName === track);
+          const artist = song ? song.ArtistName : "Unknown";
+          // Shorten the track name if it's too long
+          const song_short = track.length > 10 ? track.substring(0, 10) + '...' : track;
+          const artist_short = artist.length > 10 ? artist.substring(0, 10) + '...' : artist;
+          return `${song_short} | (${artist_short})`;
+        }))
     } else {
-      xAxis.attr("transform", `translate(${margin.left}, ${currentInnerHeight + margin.top})`).call(d3.axisBottom(xScale).tickFormat(d => d.length > 10 ? d.substring(0, 10) + '...' : d));
+      xAxis.attr("transform", `translate(${margin.left}, ${currentInnerHeight + margin.top})`).call(d3.axisBottom(xScale)
+        .tickFormat(track => {
+          const song = brushData.find(d => d.TrackName === track);
+          const artist = song ? song.ArtistName : "Unknown";
+          // Shorten the track name if it's too long
+          const song_short = track.length > 10 ? track.substring(0, 10) + '...' : track;
+          const artist_short = artist.length > 10 ? artist.substring(0, 10) + '...' : artist;
+          return `${song_short} | (${artist_short})`;
+        }))
     }
+
+    xAxis.selectAll("text")
+        .attr("transform", "rotate(-45)")
+        .style("text-anchor", "end")
+        .attr("dx", "-0.8em")
+        .attr("dy", "0.15em");
+
 
     let yAxis = svg.select(".y-axis");
     if (yAxis.empty()) {
@@ -280,12 +350,12 @@ class Dashboard extends Component {
       innerChart.append("text")
         .attr("class", "x-axis-label")
         .attr("x", currentInnerWidth / 2)
-        .attr("y", currentInnerHeight + margin.bottom)
+        .attr("y", currentInnerHeight + margin.bottom + 60)
         .attr("text-anchor", "middle")
         .text("Songs");
     } else {
       xAxisLabel.attr("x", currentInnerWidth / 2)
-                .attr("y", currentInnerHeight + margin.bottom);
+                .attr("y", currentInnerHeight + margin.bottom + 60);
     }
 
     let yAxisLabel = innerChart.select(".y-axis-label");
@@ -301,9 +371,12 @@ class Dashboard extends Component {
       yAxisLabel.attr("x", -currentInnerHeight / 2)
                 .attr("y", -margin.left / 1.5);
     }
+
+    if (!this.sliderInitialized) {
+      this.createZoomSlider(xScale);
+      this.sliderInitialized = true;
+    } 
   }
-   
-  
 
   // Handle interactive selections for scatterplot
   pick_xValue_4Scatterplot = (event) => {
@@ -330,10 +403,10 @@ class Dashboard extends Component {
       <div className="dashboard" >
         <FileUpload onDataUpload={this.handleDataUpload} />
  
-        <div style={{ display: 'flex', flexDirection: 'row', alignItems:"center"}}>
-          { !data || data.length <= 0 && <h6 style={{g: "10px"}}>Upload a file to get started</h6> }
+        <div id="theBox">
+          { !data || data.length <= 0 && <p style={{textAlign: 'center', fontStyle: 'italic',}}>Upload a file to get started</p> }
           { data.length > 0 &&
-            <div id='left-visual' width="800px" padding="20px" >
+            <div id='left-visual' style={{"width":"auto", "height":"auto", "padding":"20px"}}>
               {/** Y dropdown (Scatter) */}
               <select name="y-attr-scatter" id="y-attr-scatter" onChange={this.pick_yValue_4Scatterplot}>
               <option disabled selected value> -- select an option -- </option>
@@ -345,8 +418,8 @@ class Dashboard extends Component {
               </select> 
 
               {/** Scatterplot + slider in here */}
-              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <svg className='scatterplot'>
+              <div>
+                <svg className='scatterplot' style={{ width: '100%' }}>
                   <g className='inner_chart' />
                 </svg>
                 <svg className='slider-year'></svg>
@@ -364,18 +437,34 @@ class Dashboard extends Component {
             </div>
         }
         {/** ------------------------------------------------------ */}
-         { data.length > 0 && <div id='right-visual'>
+         { this.state.x1 && this.state.y1 && 
+         <div id='right-visual' style={{"width":"auto", "height":"auto", "padding":"20px"}}>
 
-            {/** Scatterplot + slider in here */}
-            <g>
-              <svg className='stackedChart' >
-                <g className='stacked_inner_chart' />
-              </svg>
-              <svg className='slider-streams'></svg>
-            </g>
+            {/** Stacked Bar Chart + slider in here */}
+            {bar_filtered_data.length === 0 && (
+              <p style={{ textAlign: 'center', fontStyle: 'italic',}}>
+                Use the brush tool on the scatterplot to populate this chart.
+              </p>
+            )}
+            <div style={{"width":"auto", "height":"auto"}}>
+                <svg className='stackedChart'>
+                    <g className='stacked_inner_chart'/>
+                </svg>
+                <svg className='slider-streams'></svg>
+            </div>
 
+            <div id="tooltip" style={{
+              position: "absolute",
+              background: "white",
+              border: "1px solid gray",
+              padding: "5px",
+              fontSize: "12px",
+              pointerEvents: "none",
+              opacity: 0
+            }}></div>
+            
             {/** Dropdown (Stacked) + Legend for Bar Colors*/}
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center',}}>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width:'auto', height:'auto'}}>
 
               <p>Categories</p>
               <svg className="stacked-legend" width="150" height={200}></svg>
@@ -392,31 +481,6 @@ class Dashboard extends Component {
             </div>
           </div>}
         </div>
-        {/** Will use later 
-          <div id='left-visual'>
-                    {/** X dropdown (stacked) }
-                    <select name="x-attr-stacked" id="x-attr-stacked" onChange={this.pick_xValue_4Stacked}>
-                      {dropdown_scatter.map((attr, index) => (
-                          <option key={index} value={attr.toLowerCase()}>
-                            {attr}
-                          </option>
-                      ))}
-                    </select> 
-
-                    {/** Stacked Bar Chart  in here }
-                  <svg className='stackedBar'></svg>
-                  <svg className='stream-slider'></svg>
-
-                    {/** Y dropdown (stacked) }
-                    <select name="x-attr-stacked" id="x-attr-stacked" onChange={this.pick_yValue_4Stacked}>
-                      {dropdown_scatter.map((attr, index) => (
-                          <option key={index} value={attr.toLowerCase()}>
-                            {attr}
-                          </option>
-                      ))}
-                    </select> 
-                  </div>
-          */}
 
       </div>
     );
